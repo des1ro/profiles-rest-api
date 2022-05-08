8f5b72dc032c6868c83314038c0e0d10cbc6cc35
jasio <jasio162018@gmail.com>
2022-05-08 17:41:24 +0000

    Added HelloApiViev

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,16 +1,18 @@
-from rest_framework.views import APIViev
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
-class HelloApiView(APIView):
-    """Test APII View"""
 
-    def get(self, request, format=None)
-        ""Return a list of APIView features"""
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+
         an_apiview = [
-        'Uses HTTP methods as function (get, post, patch, put, delete)',
-        'Is simlar to traditional Django View',
-        'Gives you the most control over you application logic',
-        'Is mapped manually to URLs'
+            'Uses HTTP methods as functions (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your logic',
+            'Is mapped manually to URLs',
         ]
 
-        return Response({'message':'Hello', 'an_apiview': an_apiview})
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
