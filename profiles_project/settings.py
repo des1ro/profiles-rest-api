727d7fda71a83c22807c194d9c4ea685add18c80
Jasio <jasio182016@gmail.com>
2022-05-27 18:52:59 +0200

    Configure for deployment v2

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,9 +25,8 @@ SECRET_KEY = 'y73wg(c_p(^#-3k-_9a=l0t%82yy+4zc42&h0gek%8z&u+e(f0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = ['ec2-54-225-28-118.compute-1.amazonaws.com', '127.0.0.1'
-
-]
+ALLOWED_HOSTS = ['ec2-54-225-28-118.compute-1.amazonaws.com',
+    '127.0.0.1']
 
 
 # Application definition
